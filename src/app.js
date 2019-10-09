76712b193f944a05b47f5c8fd540d1b1be80812c
iprih <priscila.silva076@gmail.com>
2019-10-09 20:20:31 -0300

    alterações

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.all("*", function(req, res, next){
+    console.log("passamos pelo app, irruuuuuuu!!!!")
+    next()
+})
 //rotas
 const index = require("./routes/index")
 const alunas = require('./routes/alunasRoute')
--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -14,6 +14,10 @@ exports.getById = (req, res) => {
 exports.getBooks = (req, res) => {
     const id = req.params.id
     const aluna = alunas.find(aluna => aluna.id == id)
+
+    if(!aluna){
+        res.send("nao localizada")
+    }
     const livros = aluna.livros //.map(test => aluna.livros)
     const titulos = livros.map(livro => livro.titulo)
     //console.log(livros)
