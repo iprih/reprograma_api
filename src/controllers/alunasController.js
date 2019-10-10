658aef32446a696418b350c3d6c643d0cfe5213f
iprih <priscila.silva076@gmail.com>
2019-10-09 22:17:20 -0300

    alteracoes api

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -8,8 +8,8 @@ exports.get = (req, res) => {
 exports.getById = (req, res) => {
     const id = req.params.id
     if (id > 17 || id <= 0) {
-       // res.send("id não é valido")
-          res.redirect(301,"https://github.com/iprih")
+      res.send("id não é valido")
+    //res.redirect(301,"https://github.com/iprih")
     }
     console.log(id)    
     res.status(200).send(alunas.find(aluna => aluna.id == id))
