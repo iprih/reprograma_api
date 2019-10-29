8015f1f32837048af8841c8431e79f52ecccc8ab
Priscila Silva <43504804+iprih@users.noreply.github.com>
2019-10-28 22:14:24 -0300

    Update alunasController.js

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -1,13 +1,9 @@
 const alunas = require("../model/alunas.json")
 
-
-
 exports.get = (req, res) => {
     console.log(req.url)
     res.status(200).send(alunas)
 }
-
-
 
 exports.getById = (req, res) => {
     const id = req.params.id
